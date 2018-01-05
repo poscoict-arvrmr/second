bce239c8bc46d8b6b332d397147cd1a173cc27b6
yujin <julyujin@nate.com>
2018-01-05 14:54:17 +0900

    initial

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-second
+[https://poscoict-arvrmr.github.io/docs/](https://poscoict-arvrmr.github.io/docs/) 를 참고하세요. 
---
+++ assets/react.js
@@ -0,0 +1,10 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <div>
+       Hello
+    </div>
+    , 
+    document.getElementById('main')
+);
