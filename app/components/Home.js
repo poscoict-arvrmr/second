37af0c3e31bc9e23e30c71244a510573308e7f7e
unknown <sosunpark@POSCOICT.AD>
2018-01-24 16:07:33 +0900

    changed loading/user icon in Home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -14,28 +14,40 @@ export default class Home extends Component<Props> {
   render() {
     console.log('[Home.js]','render', this, this.props);
     return (
-      <div className={styles.home}>
-      <div>
-        <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
-      </div>
-      <div>
-        {this.props.msg}
-      </div>
+    //login하기 전에는 로딩 아이콘, 한 후에는 사용자 아이콘 띄우기
+    //{ } 안에서 && 이후의 render파트는 하나의 큰 <div> 로 묶어줘야 함. 
+    <div className={styles.home}>
       {
         this.props.authed && 
-        <Link to="/menu">
-          <Button color="gray" onClick={() => console.log('Clicked!')} size="30" paddingLeft="20px" paddingRight="20px" margin="20px">
-          Start
-          </Button>
-        </Link>
+        <div>
+          <div>
+            <i class="fa fa-user-circle fa-3x"></i>
+          </div>
+          <div>
+            {this.props.msg}
+          </div>
+          <Link to="/menu">
+            <Button color="gray" onClick={() => console.log('Clicked!')} size="30" paddingLeft="20px" paddingRight="20px" margin="20px">
+            Start
+            </Button>
+          </Link>
+        </div>
       }
       {
         !this.props.authed && 
-        <Link to="/login">
-        <Button color="gray" size="30" paddingLeft="20px" paddingRight="20px" margin="20px">
-        Log in
-        </Button>
-        </Link>
+        <div>
+          <div>
+            <i class="fa fa-spinner fa-pulse fa-3x"></i>
+          </div>
+          <div>
+          {this.props.msg}
+          </div>
+          <Link to="/login">
+          <Button color="gray" size="30" paddingLeft="20px" paddingRight="20px" margin="20px">
+          Log in
+          </Button>
+          </Link>
+        </div>
       }
     </div>
     );
