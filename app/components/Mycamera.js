b7fea7f3d06e649adf10e187f36501afbcc7c772
unknown <sosunpark@POSCOICT.AD>
2018-02-05 13:40:37 +0900

    camera testing

--- app/components/Mycamera.js
+++ app/components/Mycamera.js
@@ -6,6 +6,8 @@ import styles from './Mymenu.css';
 //this.props로 액션 access 할 수 있음. 따로 import 할 필요 없음.
 import mqtt from "mqtt";
 import { history } from '../store/configureStore';
+import { Window, View, Box, Text, Button } from 'react-desktop/macOs';
+
 
 const client = mqtt.connect('mqtt://localhost:1883', {clientId:'camera'});
 client.on('message', (topic, message) => {
@@ -57,19 +59,93 @@ export default class Mycamera extends Component {
     client.unsubscribe('gesture/state',cb_unsubscribe);
     client.end();
   }
+
+  handleImageLoaded() {
+    this.refs.mjpeg_dest.src = 'http://192.168.246.244/html/cam_pic.php?time=' + new Date().getTime();
+  }
+
+  handleImageErrored() {
+    setTimeout(() => {
+      this.refs.mjpeg_dest.src = 'http://192.168.246.244/html/cam_pic.php?time=' + new Date().getTime();
+    }, 100);
+  }
+
+  handleRecordVideoImage() {
+    
+  }
+
+  handleRecordImage() {
+    this.refs.mjpeg_dest.src = 'http://192.168.246.244/html/cmd_pipe.php?cmd=im';
+  }
+
+  handelTimelapseStart() {
+  }
+
+  handleMotionDetectionStart() {
+  }
+
+  handleStopCamera() {
+  }
+
   render() {
     console.log("-----Mycamera component-------");
     return (
-        <div className={styles.content}>
-          <p>Take a Photo</p>
-          <p>Record a Video</p>
-          <button onClick={this.props.startRec}>Start Recording</button>
-          <button onClick={this.props.stopRec}>Stop Recording</button>
-          <p>Motion Capture</p>
-        </div>
+      <Window
+      chrome
+      height="600px"
+      padding="10px"
+      opacity="1"
+      horizontalAlignment="center"
+      verticalAlignment="center"
+      >
+        <View
+          horizontalAlignment="center"
+          verticalAlignment="center"
+          layout="vertical"
+          width="600px"
+          height="800px"
+          padding="10px"
+          
+          >
+
+          <View
+            layout="horizontal"
+            height="100px"
+          >
+            <Link to="/">
+              <i className="fa fa-arrow-left fa-3x" />
+            </Link>
+            &nbsp;
+            <Link to="/images">
+              <i className="fa fa-arrow-right fa-3x" />
+            </Link>
+          </View>
+
+          <View
+            height="380px"
+          >
+            <div>
+              <img
+                ref="mjpeg_dest"
+                onLoad={this.handleImageLoaded.bind(this)}
+                onError={this.handleImageErrored.bind(this)}
+                src="http://192.168.246.244/html/loading.jpg"
+                alt=""/>
+            </div>
+          </View>
+          <View>
+            <button onClick={this.handleRecordVideoImage.bind(this)}><i className="fa fa-play-circle fa-5x"></i></button>
+            &nbsp;&nbsp;&nbsp;
+            <button onClick={this.props.stopRec}><i className="fa fa-stop-circle fa-5x"></i></button>
+          </View>
+
+         </View>
+      </Window>
+      
     );
   }
   componentDidMount(){
+    responsiveVoice.speak("음성명령을 통하여 사진을 찍을 수 있습니다", "Korean Female");
     if(!client.connected){
       console.log('[Mycamera.js]','componentDidMount','reconnect');
       client.reconnect();
@@ -81,4 +157,15 @@ export default class Mycamera extends Component {
       client.reconnect();
     }
   }
+
 }
+
+/**
+ *   <div className={styles.content}>
+          <p>Take a Photo</p>
+          <p>Record a Video</p>
+          <button onClick={this.props.startRec}>Start Recording</button>
+          <button onClick={this.props.stopRec}>Stop Recording</button>
+          <p>Motion Capture</p>
+        </div>
+ */
